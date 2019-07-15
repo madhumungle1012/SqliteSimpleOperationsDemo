dd64480aaaa32bb5e08ae1c362ffd456482a446c
MadhuriMungle <madhurimungle1012@gmail.com>
2019-07-15 15:37:56 +0530

    first commit

---
+++ app/src/main/java/com/example/sqlitesimpleoperationsdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sqlitesimpleoperationsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+
+    }
+}
